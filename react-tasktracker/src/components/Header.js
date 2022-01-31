e545bd2a1f373fe31963c3b32ede55b0a98b27e6
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-01-31 16:11:54 -0500

    Changed index.css

--- react-tasktracker/src/components/Header.js
+++ react-tasktracker/src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-        <h1 style={headingStyle}>{title}</h1>
+        <h1>{title}</h1>
     </header>
   )
 }
