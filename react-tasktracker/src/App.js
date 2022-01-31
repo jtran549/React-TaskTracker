cdb3ec4d5067cb656228bc6011ee2d855efd5e11
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-01-31 17:56:10 -0500

    Added task component

--- react-tasktracker/src/App.js
+++ react-tasktracker/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks'
 
 function App() {
   const name = "Jimmy"
   return (
     <div className="Container">
       <Header />
+      <Tasks />
     </div>
   );
 }
