3cf8f8b6e9f44de655e0b4b26b5ed291d1d59344
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-01-31 15:58:10 -0500

    Added title props to Header component

--- react-tasktracker/src/components/Header.js
+++ react-tasktracker/src/components/Header.js
@@ -1,10 +1,18 @@
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-        <h1>Task Tracker</h1>
+        <h1>{title}</h1>
     </header>
   )
 }
 
+Header.defaultProps = {
+    title: 'Task tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
 export default Header
