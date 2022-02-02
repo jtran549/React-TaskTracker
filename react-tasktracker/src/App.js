71d5bda09789dec4dc901b4d5299990612156649
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-02-01 20:39:43 -0500

    Move tasks to useState
    Changed tasks from being a hardcoded array of objects in the Tasks component to useState in the app component

--- react-tasktracker/src/App.js
+++ react-tasktracker/src/App.js
@@ -1,12 +1,33 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Groceries',
+        day: 'Feb 6th at 5:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Lay pipe',
+        day: 'Feb 7th at 10:30pm',
+        reminder: true
+    }
+]);
   const name = "Jimmy"
   return (
     <div className="Container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
