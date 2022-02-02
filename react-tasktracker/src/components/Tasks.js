f32909c26bcbac22dc83a9043a4fdf723ed5bc96
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-02-01 20:47:44 -0500

    Changed tasks component to use task component

--- react-tasktracker/src/components/Tasks.js
+++ react-tasktracker/src/components/Tasks.js
@@ -1,28 +1,11 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Groceries',
-        day: 'Feb 6th at 5:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Lay pipe',
-        day: 'Feb 7th at 10:30pm',
-        reminder: true
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({tasks}) => {
   return (
     <>
-      {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+      {tasks.map((task) => 
+        (<Task key={task.id} task={task}/>)
+    )}
     </>
   )
 }
